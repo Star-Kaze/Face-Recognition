f4f9c67bd78ade4cfd8a0a3b6272e17eccec41ab
LamLam <nguyenbaolamss030205@gmail.com>
2024-11-22 01:35:19 +0700

    haar done

--- src/main.py
+++ src/main.py
@@ -49,6 +49,9 @@ class Main(QMainWindow):
         self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         self.frame_skip = 2  # Process every 2nd frame
         self.frame_count = 0
+
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
         timer = QTimer(self)
         timer.timeout.connect(self.show_webcam)
         timer.start(10)
@@ -59,31 +62,32 @@ class Main(QMainWindow):
         if ret:
             self.frame_count += 1
             if self.frame_count % self.frame_skip == 0:
-                cam_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                face_locations = face_recognition.face_locations(cam_image, model="hog")
+                gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = self.face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
                 largest_face_area = 0
                 largest_face = None
-                if face_locations:
-                    for (top, right, bottom, left) in face_locations:
-                        face_area = (right - left) * (bottom - top)
+                if len(faces) > 0:
+                    for (x, y, w, h) in faces:
+                        face_area = w * h
 
-                    # Update largest face if this has a bigger area
-                    if face_area > largest_face_area:
-                        largest_face_area = face_area
-                        largest_face = (top, right, bottom, left)
+                        # Update largest face if this has a bigger area
+                        if face_area > largest_face_area:
+                            largest_face_area = face_area
+                            largest_face = (x, y, w, h)
 
                     # If a largest face is found, draw rectangle and potentially capture it
                     if largest_face:
-                        top, right, bottom, left = largest_face
-                        cv2.rectangle(cam_image, (left, top), (right, bottom), (0, 255, 0), 2)
+                        x, y, w, h = largest_face
                         # add images to image_list
-                        self.image_list["cut_face"] = cam_image[top:bottom,left:right]
-                        self.image_list["full_image"] = cam_image
+                        self.image_list["cut_face"] = frame[y:y + h,x:x + w]
+                        self.image_list["full_image"] = frame
                         self.faces_exist = True
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 else:
                     self.faces_exist = False
 
-                qimg = QImage(cam_image.data, cam_image.shape[1], cam_image.shape[0], QImage.Format.Format_RGB888)
+                rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                qimg = QImage(rgb_frame.data, rgb_frame.shape[1], rgb_frame.shape[0], QImage.Format.Format_RGB888)
                 qpix = QPixmap.fromImage(qimg)
                 self.cam_label.setPixmap(qpix)
 
@@ -94,6 +98,8 @@ class Main(QMainWindow):
             cv2.imwrite('./.cut_face.jpg', cut_face_rgb)
             cv2.imwrite('./.full_image.jpg', full_image_rgb)
             print("Photos saved successfully")
+            print("Cut face color format:", cut_face_rgb.shape)
+            print("Full image color format:", full_image_rgb.shape)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
